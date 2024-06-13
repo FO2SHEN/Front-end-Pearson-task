b10a75ee96e0f3f240d3d7935102b36b3cd29f51
yousef <youseffouad637@gmail.com>
2024-06-13 20:39:43 +0300

    ..

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,22 +11,22 @@ function Contact() {
             <div className="row">
               <div className="col-6">
                 <label>Name</label>
-                <input type="text" className="form-control" placeholder="Name"/>
+                <input type="text" className="form-control" placeholder="Name" required/>
               </div>
               <div className="col-6">
                 <label>Email</label>
-                <input type="email" className="form-control" placeholder="Email"/>
+                <input type="email" className="form-control" placeholder="Email" required/>
               </div>
             </div>
 
             <div className='col-8'>
                 <label>Subject</label>
-                <input type="text" className="form-control" placeholder="Subject"/>
+                <input type="text" className="form-control" placeholder="Subject" required/>
             </div>
 
             <div className='col-8'>
               <label>Message</label>
-              <textarea className="form-control" id="exampleFormControlTextarea1"></textarea>
+              <textarea className="form-control" id="exampleFormControlTextarea1" required></textarea>
             </div><br />
             <button type='submit' className="btn btn-primary col-2">Send</button>
             
